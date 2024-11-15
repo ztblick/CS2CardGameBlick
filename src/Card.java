e1eb4e1137e4c26ab9b903c7d3050c1305306335
Zachary Blickensderfer <zach.blickensderfer@gmail.com>
2024-11-15 13:51:08 -0800

    Built card class.

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
 public class Card {
+
+    private String rank;
+    private String suit;
+    private int value;
+
+    public Card(String rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
